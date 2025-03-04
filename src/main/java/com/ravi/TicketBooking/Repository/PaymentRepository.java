1cb1713e8834596471c4976e43d60ebfc95e7a99
ravi0031 <ravikirangumma3@gmail.com>
2025-03-04 08:54:28 +0530

    Add files via upload

--- src/main/java/com/ravi/TicketBooking/Repository/PaymentRepository.java
+++ src/main/java/com/ravi/TicketBooking/Repository/PaymentRepository.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ravi.TicketBooking.Entity.User;
-
-
+import com.ravi.TicketBooking.Entity.Movie;
+import com.ravi.TicketBooking.Entity.Payment;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
 
-	List<User> findByPhoneNumber(String phoneNumber);
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+	List<Payment> findAll();
 }
